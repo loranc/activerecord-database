334aed82ac691d2d944766f2cbccd84894d34f61
Your Actual Name <laurentcuzin@gmail.com>
2014-01-27 15:05:38 +0100

    soluce interacting with a database

--- 01.Interacting_with_code/main.rb
+++ 01.Interacting_with_code/main.rb
@@ -5,8 +5,9 @@ database_path = "db/jukebox.sqlite"
 db = SQLite3::Database.new(database_path)
 
 def number_of_rows(db,table_name)
-  # your code here
+  db.execute ("SELECT COUNT(*) FROM #{table_name}")
 end
+#SQL request should be wrapped into a string. 
 
 puts "There are #{number_of_rows(db,"Artist")} artists"
 puts "There are #{number_of_rows(db,"Track")} tracks"
@@ -16,10 +17,15 @@ puts "There are #{number_of_rows(db,"MediaType")} media types"
 
 
 # returns all the artists sorted by alphabetical order.
-artists = # your code here
+artists = db.execute ("SELECT Name FROM Artist ORDER BY Name")
+puts artists
 
 # returns all the love songs
-love_songs = # your code here
+love_songs = db.execute ("SELECT Name FROM Track WHERE Name LIKE '%love%' ")
+puts love_songs
 
 # returns all the tracks that are longer than 5 minutes.
-long_tracks = # your code here
+long_tracks = db.execute ( "SELECT Name,Milliseconds FROM Track WHERE Milliseconds > 3000000 " )
+puts long_tracks
+
+#possibility to select more than on table (here Name, Milliseconds)
