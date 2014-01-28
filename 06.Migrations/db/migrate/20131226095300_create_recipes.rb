67a60167b9d85bedf4240d301e969da71af1b8d3
Your Actual Name <laurentcuzin@gmail.com>
2014-01-28 18:07:43 +0100

    migration

--- 06.Migrations/db/migrate/20131226095300_create_recipes.rb
+++ 06.Migrations/db/migrate/20131226095300_create_recipes.rb
@@ -1,5 +1,10 @@
 class CreateRecipes < ActiveRecord::Migration
   def up
-    # your code here to create the Recipes table
+    create_table :Recipes do |t|
+      t.string :name
+      t.string :description
+      t.integer :length
+      t.integer :difficulty
+    end
   end
 end
